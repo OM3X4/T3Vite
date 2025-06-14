ce937d2cf0fd162ede6c0c8d3bed56ff1b16b27e
OM3X4 <om3x4e@gmail.com>
2025-06-14 23:28:27 +0300

    Fix import case for Loading component in Settings

--- Client/src/Components/Settings.tsx
+++ Client/src/Components/Settings.tsx
@@ -2,7 +2,7 @@ import { FiLogOut } from "react-icons/fi";
 import { AiOutlineArrowLeft } from "react-icons/ai";
 import { useEffect, useState, useRef } from 'react'
 import useUserData from '../hooks/useUserData'
-import Loading from './loading'
+import Loading from './Loading'
 import useUpdateData from '../hooks/useUpdateData'
 import useUpdateKey from '../hooks/useUpdateKey'
 import { Link } from "react-router";
