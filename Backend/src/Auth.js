a93bd299e3d4790d4194bcc21f30491fb4005f2b
OM3X4 <om3x4e@gmail.com>
2025-06-18 14:05:58 +0300

    Remove unused authentication and UI components; clean up environment variables and add MIT license

--- Backend/src/Auth.js
+++ Backend/src/Auth.js
@@ -1,6 +1,4 @@
 import { Router } from "express";
-import passport from "./auth/google.js";
-import { generateToken } from "./utils/jwt.js";
 import authenticateJWT from './middleware/AuthenticateUser.js';
 import { encrypt } from "./utils/crypto.js";
 import { PrismaClient } from "@prisma/client";
@@ -8,30 +6,6 @@ import { PrismaClient } from "@prisma/client";
 
 const router = Router();
 const prisma = new PrismaClient();
-
-// router.get('/google', passport.authenticate('google', {
-//     scope: ['profile', 'email'],
-// }));
-
-// router.get('/google/callback',
-//     passport.authenticate('google', { session: false }),
-//     (req, res) => {
-//         const user = req.user;
-//         const token = generateToken({ id: user.id, email: user.email });
-
-//         const isProd = process.env.NODE_ENV === 'production';
-
-//         res.cookie('token', token, {
-//             httpOnly: false,
-//             secure: true,
-//             sameSite: "none",
-//             maxAge: 7 * 24 * 60 * 60 * 1000,
-//         });
-
-//         // Optional: Redirect to your frontend (e.g. dashboard)
-//         res.redirect(`${process.env.CLIENT_URL}/`);
-//     }
-// );
 
 
 router.post("/key", authenticateJWT, async (req, res) => {
--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -4,7 +4,6 @@ import './index.css'
 import App from './App.tsx'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { TooltipProvider } from './Components/ui/tooltip.tsx'
 
 const queryClient = new QueryClient()
 
@@ -12,9 +11,7 @@ createRoot(document.getElementById('root')!).render(
   <Router>
     <StrictMode>
       <QueryClientProvider client={queryClient}>
-        <TooltipProvider>
           <App />
-        </TooltipProvider>
       </QueryClientProvider>
     </StrictMode>
   </Router>
