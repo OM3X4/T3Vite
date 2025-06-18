b60ac2096f83abfb1a92960df7a66df439cf7615
OM3X4 <om3x4e@gmail.com>
2025-06-18 14:21:53 +0300

    Update Supabase client configuration to use environment variables for project URL and anon key

--- Client/src/hooks/supabaseClient.ts
+++ Client/src/hooks/supabaseClient.ts
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js'
 
 // Replace with your actual Supabase project URL and anon/public key
 export const supabase = createClient(
-    'https://ryxipipmkchhdmirludf.supabase.co',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ5eGlwaXBta2NoaGRtaXJsdWRmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk1NjkzMDMsImV4cCI6MjA2NTE0NTMwM30.erFEuoyJNsqI8jer2Z0WKIUyFcr4JKSEq7BzvZrfGFk'
+    import.meta.env.VITE_SUPABASE_PROJECT_URL,
+    import.meta.env.VITE_SUPABASE_ANON_KEY
 )
